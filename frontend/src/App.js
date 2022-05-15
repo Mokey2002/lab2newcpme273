82f0c41c62bfb6d6fe9cc15607f0e2e3f2568a8b
Eduardo Lemus <edventura91@gmail.com>
2022-05-14 22:12:31 -0700

    added all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Router, Switch, Route, Link } from "react-router-dom";
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
